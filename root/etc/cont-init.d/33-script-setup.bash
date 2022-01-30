9c6fddd91c86e154d4e200728dbe093167576ce1
Protektor <tim.w.jung@gmail.com>
2022-01-30 15:41:31 -0600

    Create 33-script-setup.bash

---
+++ root/etc/cont-init.d/33-script-setup.bash
@@ -0,0 +1,51 @@
+#!/usr/bin/with-contenv bash
+
+# Create Scripts Directory
+if [ ! -d "/config/scripts" ]; then
+	mkdir "/config/scripts"
+fi
+
+# Remove existing script
+if [ -f "/config/scripts/MKVTagger.bash" ]; then
+	rm "/config/scripts/MKVTagger.bash"
+fi
+
+# import script
+if [ ! -f "/config/scripts/MKVTagger.bash" ]; then
+	cp "/scripts/MKVTagger.bash" "/config/scripts/MKVTagger.bash"
+fi
+
+# Remove existing script
+if [ -f "/config/scripts/SMA.bash" ]; then
+	rm "/config/scripts/SMA.bash"
+fi
+
+# import script
+if [ ! -f "/config/scripts/SMA.bash" ]; then
+	cp "/scripts/SMA.bash" "/config/scripts/SMA.bash"
+fi
+
+# Remove existing script
+if [ -f "/config/scripts/MetadataWriter.bash" ]; then
+	rm "/config/scripts/MetadataWriter.bash"
+fi
+
+# import script
+if [ ! -f "/config/scripts/MetadataWriter.bash" ]; then
+	cp "/scripts/MetadataWriter.bash" "/config/scripts/MetadataWriter.bash"
+fi
+
+# Remove existing script
+if [ -f "/config/scripts/Mass-MetadataWriter.bash" ]; then
+	rm "/config/scripts/Mass-MetadataWriter.bash"
+fi
+
+# import script
+if [ ! -f "/config/scripts/Mass-MetadataWriter.bash" ]; then
+	cp "/scripts/Mass-MetadataWriter.bash" "/config/scripts/Mass-MetadataWriter.bash"
+fi
+
+# set permissions
+chmod 0777 -R "/config/scripts"
+
+exit $?
